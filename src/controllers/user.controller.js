a937ea6add7f07b717deb557cdc3dcc96fb51bd7
Adil Wagay <adilwagay@Adils-MacBook-Air.local>
2026-08-06 19:42:51 +0530

    Add regsiter controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,59 @@
 import { asyncHandler} from "../utils/asyncHandler.js"
-
+import { ApiError} from "../utils/apiError.js"
+import { User } from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/apiResponse.js"
 
 const registerUser = asyncHandler( async (req, res) =>{
-    res.status(200).json({
-        message: "ok"
+    const {fullName, email, username, password} = req.body
+    console.log("email",email)
+    if (
+        [fullName, username, email, password].some((field) =>
+        field?.trim()=="")
+    ) {
+        throw new ApiError(400, "All Fields Are Required")
+    }
+    const existedUser = User.findOne({
+        $or: [{username},{email}]
     })
+    if (existedUser){
+        throw new ApiError(409, "user already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    if (!avatarLocalPath){
+        throw new ApiError(400,"avatar is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar){
+        throw new ApiError(400,"avatar is required")
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    if (!createdUser) {
+        throw new ApiError(500, "something went wrong while registering the user")
+
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"user registerd successfully"
+        )
+    )
 })
 
 export {registerUser}
